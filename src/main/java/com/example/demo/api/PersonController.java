0a2a389d3954d9937f104d3d50eeadc2b8f477dc
Tristan Elliott <>
2021-02-12 18:47:27 -0400

    main endpoint

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,14 +3,17 @@ package com.example.demo.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 
+
 @RestController
-@RequestMapping("/design")
+@RequestMapping("/api/v1/person")
 public class PersonController {
 	private final PersonService personService;
 	
@@ -20,8 +23,8 @@ public class PersonController {
 	}
 
 	
-		@GetMapping
-		public void addPerson(Person person) {
+		@PostMapping
+		public void addPerson(@RequestBody Person person) {
 			personService.addPerson(person);
 		}
 		
