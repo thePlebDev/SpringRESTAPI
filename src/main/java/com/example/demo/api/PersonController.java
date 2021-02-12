9eb3ddd17f73aee9131ca93c5fb4b217f818bba9
Tristan Elliott <>
2021-02-12 17:52:05 -0400

    Autowiring stuff up

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -1,18 +1,28 @@
 package com.example.demo.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+
 @RestController
 @RequestMapping("/design")
 public class PersonController {
+	private final PersonService personService;
+	
+	@Autowired
+	public PersonController(PersonService personService) {
+		this.personService = personService;
+	}
 
 	
 		@GetMapping
-		public void talk() {
-			System.out.println("it do be like that");
+		public void addPerson(Person person) {
+			personService.addPerson(person);
 		}
 		
 	
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.model.Person;
 
-
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
 	
 	private static List<Person> DB = new ArrayList<>();
